877e30f5dc332faa9a584b17e260dd07a2c6e467
Marc DEXET <marcdexet@gmail.com>
2013-08-30 12:16:04 +0200

    Structure de base et première classe

---
+++ src/main/java/jdev/Person.java
@@ -0,0 +1,23 @@
+package jdev;
+
+public class Person {
+
+	private String forname;
+	private String lastname;
+	
+	
+	public String getForname() {
+		return forname;
+	}
+	public void setForname(String forname) {
+		this.forname = forname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	
+	
+}
